8574d807dc2960de0f264fe1f943ae64fbba3271
Rob <robertdacorte@gmail.com>
2023-06-21 19:36:03 +0200

    Initial commit

---
+++ Hello World/Hello_WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_WorldApp.swift
+//  Hello World
+//
+//  Created by Rob on 21/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Hello_WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
