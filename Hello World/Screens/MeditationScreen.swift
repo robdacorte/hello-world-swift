c4c3f0830bc41b6e47f26bfd69a8b1fbf22fe9f3
Rob <robertdacorte@gmail.com>
2024-01-12 22:25:28 +0100

    Refactor meditation view, added timer and video player

--- Hello World/Screens/MeditationScreen.swift
+++ Hello World/Screens/MeditationScreen.swift
@@ -6,67 +6,69 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct MeditationScreen: View {
+    //MARK: - Properties
     @ObservedObject var vm: MeditationViewModel = MeditationViewModel()
-    
+    //MARK: - Body
     var body: some View {
-            ZStack {
-                TimelineView(.animation) { timeline in
-                    MeditationView(startTime: $vm.time, timeline: timeline)
-                        .opacity(vm.isOn ? 1.0 : 0.0)
-                }
+        ZStack {
+            VideoPlayer(player: vm.player)
+                .aspectRatio(contentMode: .fill)
+                .ignoresSafeArea()
+                .navigationBarBackButtonHidden()
+            Color.black.opacity(vm.isActive ? 0.2 : 0.5)
+                .ignoresSafeArea()
+            ProgressCircleView(model: .init(color: .white,size: vm.isActive ? 200 : 270, percentage: vm.getPercentage(), time: vm.getUpdatedTime()))
+                .padding(.bottom, vm.isActive ? 500 : 0)
+                .animation(.smooth, value: vm.isActive)
+            VStack {
                 VStack {
                     HStack {
-                        Button {
-                            vm.togglePower()
-                        } label: {
-                            Image(systemName: vm.isOn ? "lightbulb.slash.fill" : "lightbulb.max.fill")
-                                .frame(width: 30)
-                            .padding(1)
-                            .foregroundColor(.white)
-                            .fontWeight(.bold)
-                        }
-                        .clipShape(Circle())
-                        .buttonStyle(.bordered)
-                        
                         Spacer()
                         Button {
                             
                             withAnimation(.easeInOut(duration: 1.2)) {
-                                vm.togglePicker()
+                                //                                vm.togglePicker()
                             }
                         } label: {
                             Image(systemName: "stopwatch")
-                            .frame(width: 30)
-                            .padding(1)
-                            .foregroundColor( vm.isOn ? .yellow : .gray)
-                            .fontWeight(.bold)
+                                .frame(width: 30)
+                                .padding(1)
+                            //                            .foregroundColor( vm.isOn ? .yellow : .gray)
+                                .fontWeight(.bold)
                         }
                         .clipShape(Circle())
                         .buttonStyle(.bordered)
                     }
                     .padding(.horizontal, 10)
                     Spacer()
-                    
-                    if vm.isTimerPickerOn {
-                            Picker("Choose the meditation length", selection: $vm.meditationLength) {
-                                Text("2 seg").tag(Double(2))
-                                Text("5 seg").tag(Double(5))
-                                Text("10 seg").tag(Double(10))
-                                ForEach(1..<10) { number in
-                                    Text("\(number) min").tag(Double(number * 60))
-                                }
-                            }.pickerStyle(.wheel)
-                        
-                    }
+                }
+                VStack(alignment: .leading) {
+                    Spacer()
+                    Button(action: {
+                        if vm.isActive {
+                            vm.stopTimer()
+                        } else {
+                            vm.startTimer()
+                        }
+                    }, label: {
+                        Label(vm.isActive ? "Stop" : "Start", systemImage: "timer")
+//                            .frame(maxWidth: .infinity)
+                    })
+                    .buttonStyle(.borderedProminent)
+                    .controlSize(.large)
+                    .clipShape(Capsule())
+                    .padding(.horizontal, 30)
+                    Spacer()
                 }
                 
-            
+            }
         }
     }
 }
 
 #Preview {
-        MeditationScreen()
+    MeditationScreen()
 }
--- Hello World/ViewModels/MeditationViewModel.swift
+++ Hello World/ViewModels/MeditationViewModel.swift
@@ -6,44 +6,73 @@
 //
 
 import SwiftUI
+import Combine
+import AVFoundation
 
-class MeditationViewModel: NSObject, ObservableObject {
-    var time: Date = .now
-    var timer: Timer?
+class MeditationViewModel: ObservableObject {
+    //MARK: - Properties
+    @Published var minutes: Int = 1
+    @Published var seconds: Int = 20
+    @Published var totalSeconds: Int = 0
+    @Published var isActive: Bool = false
+    @Published var player: AVPlayer = AVPlayer(url: Bundle.main.url(forResource: "ocean_waves_loop", withExtension: "mov")!)
+    private var isPlayerPlayingReverse: Bool = false
+    private var staticTotalSeconds: Int = 0
+    private var cancellable = Set<AnyCancellable>()
     
-    @Published var isOn: Bool = false
-    @Published var volume: Float = 0
-    
-    @Published var isTimerPickerOn: Bool = false
-    @Published var meditationLength: Double = 120.0
-    @Published var numberOfAlerts: Int = 1
-    
-    func togglePicker() -> Void {
-        self.isTimerPickerOn.toggle()
+    //MARK: - Functions
+    func startTimer(){
+        withAnimation {
+            isActive = true
+        }
+        totalSeconds = (minutes * 60) + seconds
+        staticTotalSeconds = totalSeconds
+        Timer.publish(every: 1, on: .main, in: .common).autoconnect().sink(receiveValue: { [weak self] _ in
+            self?.updateTimer()
+        }).store(in: &cancellable)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(videoDidFinish),
+                                               name: .AVPlayerItemDidPlayToEndTime,
+                                               object: player.currentItem)
+        player.play()
     }
     
-    func togglePower() -> Void {
-        self.isOn = !self.isOn
-        
-        
-        
-        timer = Timer.scheduledTimer(withTimeInterval: TimeInterval(meditationLength), repeats: false) { timer in
-            print("desde el callback")
-            self.isOn = false
-            self.volume = 0.9
-//             timer.invalidate()
-            print(timer)
+    func updateTimer(){
+        if !isActive {
+            stopTimer()
+            return
         }
-        
-        
-        
-        
-        
-        if self.isOn {
-            self.volume = 1.1
-        } else {
-            self.volume = 0.9
-        }
+        totalSeconds -= 1
+        minutes = (totalSeconds / 60) % 60
+        seconds = totalSeconds % 60
+        isActive = totalSeconds != 0
     }
     
+    func stopTimer() {
+        isActive = false
+        player.pause()
+        player.seek(to: .zero)
+        minutes = 1
+        seconds = 20
+        NotificationCenter.default.removeObserver(self, name: .AVPlayerItemDidPlayToEndTime, object: player.currentItem)
+        cancellable.removeAll()
+    }
+    
+    func getUpdatedTime() -> String{
+        isActive ? "\(minutes >= 10 ? "\(minutes)" : "0\(minutes)"):\(seconds >= 10 ? "\(seconds)" : "0\(seconds)")" : "00:00"
+    }
+    
+    func getPercentage() -> CGFloat {
+        isActive ? (CGFloat(totalSeconds * 100) / CGFloat(staticTotalSeconds)) / 100 : 0
+    }
+    
+    @objc private func videoDidFinish(notification: Notification) {
+        if notification.object is AVPlayerItem {
+            withAnimation(.smooth) {
+                player.playImmediately(atRate: isPlayerPlayingReverse ? 1.0 : -1.5)
+                isPlayerPlayingReverse.toggle()
+            }
+        }
+    }
 }
